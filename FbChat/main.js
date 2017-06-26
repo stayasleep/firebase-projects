0850de8c819572b646305a93a18191b883140d84
stayasleep <virtuallyinfekted@gmail.com>
2017-06-25 21:31:30 -0700

    small working demo of fb chat, using same principle as fb sgt

---
+++ FbChat/main.js
@@ -0,0 +1,57 @@
+
+firebase.initializeApp(config);
+var fbRef = firebase.database();
+
+document.addEventListener("DOMContentLoaded",ready);
+
+function ready(){
+    //var shorthand to access DOM elements
+var userName = document.getElementById("username");
+var msgInput = document.getElementById("message");
+var submitBtn = document.getElementById("submit");
+//apply event listerners
+submitBtn.addEventListener("click",function(){
+    var sentFrom = userName.value;
+    var sentMsg = msgInput.value;
+    //send it to firebase
+    // Fb.set(sentFrom + ": "+sentMsg);
+    var newMsgRef = fbRef.ref('messages').push();
+    newMsgRef.set({
+        "username":sentFrom,
+        "message":sentMsg
+    })
+    msgInput.value="";
+});
+// startListening();
+}   
+
+// function startListening(){
+    fbRef.ref('messages').on('value',function(snap){
+        //listens when the value in db changes and updates the dom
+        //save obj from db
+        var msg = snap.val();
+        console.log('this is your snap', msg);
+        
+        //obj comes back with unique id..have to go one level deeper
+        document.getElementById("results").innerHTML=""; //reset the dom so we dont get doubles
+        for(var x in msg){
+            if(msg.hasOwnProperty(x)){
+                console.log('close',msg[x]);
+                var msgUsername = document.createElement("p");
+                msgUsername.innerText = msg[x].username+": "+msg[x].message;
+
+                // var msgText = document.createElement("p");
+                // msgText.textContent = msg[x].message;
+
+                // msgUsername.appendChild(msgText);
+
+                var msgContainer = document.createElement("div");
+                msgContainer.appendChild(msgUsername);
+                // msgContainer.appendChild(msgText);
+
+                //attach it to dom
+                document.getElementById("results").appendChild(msgContainer);
+            }
+        }
+    })
+// }
